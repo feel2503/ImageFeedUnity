914cc8f85a848d9ddf75abcc6e3244b0006433bf
phill.kim <phill.kim@pointmobile.co.kr>
2019-07-13 11:00:36 +0900

    permission granted button position changed

--- FeedTestApp/app/src/main/java/com/feed/plugin/ProfilePhotoActivity.java
+++ FeedTestApp/app/src/main/java/com/feed/plugin/ProfilePhotoActivity.java
@@ -1,11 +1,18 @@
 package com.feed.plugin;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -38,6 +45,11 @@ public class ProfilePhotoActivity extends AppCompatActivity{
 
     private int REQUEST_GALLERY_SELECT = 0x1011;
 
+    private final int REQEUST_PERFMSSION_CODE = 0x1001;
+    private int REQUEST_SETTING_FOR_PERMISSION = 0x1003;
+    private String[] REQUIRED_PERMISSIONS  = {Manifest.permission.CAMERA, // 카메라
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};  // 외부 저장소
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -47,6 +59,15 @@ public class ProfilePhotoActivity extends AppCompatActivity{
 
         setContentView(R.layout.activity_profile_photo);
 
+        boolean isPermission = checkVerify();
+        if(isPermission)
+        {
+            init();
+        }
+    }
+
+    private void init()
+    {
         TextView textTitle = findViewById(R.id.text_title);
         textTitle.setTypeface(Typeface.createFromAsset(getAssets(), "RingsideWide-Semibold.otf"));
         textTitle.setText(getString(R.string.camera));
@@ -66,7 +87,30 @@ public class ProfilePhotoActivity extends AppCompatActivity{
 
 
         findViewById(R.id.btn_back).setOnClickListener(mOnClickListener);
+    }
 
+    public boolean checkVerify()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                    checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                    checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED )
+            {
+                // Should we show an explanation?
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                {
+                    // ...
+                }
+                requestPermissions(REQUIRED_PERMISSIONS,  REQEUST_PERFMSSION_CODE);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return true;
     }
 
     @Override
@@ -81,7 +125,8 @@ public class ProfilePhotoActivity extends AppCompatActivity{
     @Override
     public void onPause(){
         super.onPause();
-        mCameraView.stop();
+        if(mCameraView != null)
+            mCameraView.stop();
     }
 
     @Override
@@ -104,6 +149,56 @@ public class ProfilePhotoActivity extends AppCompatActivity{
         UnityPlayer.UnitySendMessage("FeedModule", "SetFacePhotoPath", "BACK");
     }
 
+    public void showRequestAgainDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ProfilePhotoActivity.this);
+        builder.setMessage(getString(R.string.requestpermission));
+        builder.setPositiveButton(getString(R.string.setting), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try{
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:" + getPackageName()));
+                    startActivityForResult(intent, REQUEST_SETTING_FOR_PERMISSION);
+                    //startActivity(intent);
+                }catch (ActivityNotFoundException ae)
+                {
+                    ae.printStackTrace();
+
+                    Intent intent = new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
+                    startActivity(intent);
+                }
+            }
+        });
+        builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        builder.create().show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == REQEUST_PERFMSSION_CODE && grantResults.length == REQUIRED_PERMISSIONS.length)
+        {
+            boolean check_result = true;
+            // 모든 퍼미션을 허용했는지 체크합니다.
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+            if ( check_result ) {
+                init();
+            }
+            else {
+                showRequestAgainDialog();
+            }
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
@@ -119,6 +214,22 @@ public class ProfilePhotoActivity extends AppCompatActivity{
             if(resultCode == RESULT_OK)
             {
                 finish();
+            }
+        }
+        else if(requestCode == REQUEST_SETTING_FOR_PERMISSION)
+        {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                        checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+                        checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED )
+                {
+                    init();
+                }
+                else
+                {
+                    finish();
+                }
             }
         }
     }
